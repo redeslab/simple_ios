b6a1a0b7c52534d4fc51838c239ad2530a10d453
hopwesley <ws@sinoc.io>
2022-07-14 18:57:06 +0800

    need to finish

---
+++ src/object/RuleManager.swift
@@ -0,0 +1,19 @@
+//
+//  RuleManager.swift
+//  extension
+//
+//  Created by wesley on 2022/7/14.
+//  Copyright © 2022 hyperorchid. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Simple
+
+
+class RuleManager:NSObject{
+        
+        public static func loadRules(){
+                
+        }
+}
